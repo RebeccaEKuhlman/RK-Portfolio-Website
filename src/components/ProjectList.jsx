c864c963483964d17e0e90afbd7d798007706d65
RebeccaEKuhlman <rkuhlman@smu.edu>
2022-06-23 13:39:31 -0500

    Project Collapse Implemented

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -4,53 +4,49 @@ import { useState, useContext} from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import {TopNav} from "./TopNav"
 import {BottomNav} from "./BottomNav"
+import useCollapse from 'react-collapsed';
 
 export const ProjectList = () => { 
-    const [projects, setProjects] = useState(undefined);
-    var showMore1 = false;
-    const check = (show) =>{if(show){return <p>wow!</p>}}
+  const { getCollapseProps, getToggleProps, isExpanded } = useCollapse();
+    //const [projects, setProjects] = useState(undefined);
     /**
      * Add search bar, bar to keep track of where the user is in the page with scroll links
      */
     return <div>
-        <TopNav/>
-        {/**<Autocomplete
-            disablePortal
-            id="combo-box-demo"
-            options={top100Films}
-            sx={{ width: 300 }}
-            renderInput={(params) => <TextField {...params} label="Movie" />}
-/>*/}
-            <ul>
-            {/**projects.map((project) =>
-                <li>
-                    <ProjectDetails value={project}/>
-                </li>
-)*/}
-    <div class="card w-75 bg-light p-2 m-3">
-    <img src='../bqkLogo' class="card-img-top" alt="Photo of the project"/>
-      <div class="card-body">
-        <h5 class="card-title">DIG Haunted House</h5>
-        <h6 class="card-subtitle mb-2 text-muted">September-October 2021</h6>
-        <p class="card-text">We built a haunted house, twas epic
-</p>
-      </div>
-    </div>
-    <div class="card w-75 bg-light p-2 m-3">
-    <img src='../bqkLogo' class="card-img-top" alt="Photo of the project"/>
-      <div class="card-body">
-        <h5 class="card-title">DIG Haunted House</h5>
-        <h6 class="card-subtitle mb-2 text-muted">September-October 2021</h6>
-        <p class="card-text">We built a haunted house, twas epic
-</p>
-    {
-        check(showMore1)
-    }
-    {<button onClick={showMore1 = !showMore1}/>}
-      </div>
-    </div>
+              <TopNav/>
+                        {/**<Autocomplete
+                            disablePortal
+                            id="combo-box-demo"
+                            options={top100Films}
+                            sx={{ width: 300 }}
+                            renderInput={(params) => <TextField {...params} label="Movie" />}
+                />*/}
+              <ul>
+                {/**projects.map((project) =>
+                    <li>
+                        <ProjectDetails value={project}/>
+                    </li>)*/}
+              <div class="card w-75 bg-light p-2 m-3">
+              <img src='../bqkLogo' class="card-img-top" alt="Photo of the project"/>
+                <div class="card-body">
+                  <h5 class="card-title">DIG Haunted House</h5>
+                  <h6 class="card-subtitle mb-2 text-muted">September-October 2021</h6>
+                  <p class="card-text">The DIG's Annual haunted house project. The DIG team makes almost all of the costumes, props, and décor for the haunted house, and they serve as the actors during it. All semester we independently take on tasks to be creative in getting our parts done, then we come together as a team to put it all together!
+                  </p>
+                  <div className="collapsible">
+                      <div className="header" {...getToggleProps()}>
+                          <button>{isExpanded ? 'Collapse' : 'Expand'}</button>
+                      </div>
+                      <div {...getCollapseProps()}>
+                          <div className="content">
+                          I worked on a lot of the parts requiring the vinyl cutter, gaining a new appreciation of its potential as tool. Playing with the utilization of space, and drawing the audience to look at a certain places in the maze was very fun and informative!
+                          </div>
+                      </div>
+                  </div>
+                </div>
+              </div>
             </ul>
             <span className="fixed-bottom"><BottomNav /></span>
-    </div>
+            </div>
 };
 
